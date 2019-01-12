23c602589e2e4a2523c6be5aebe49d4d2f53595c
arie.cattan <arie.cattan@gmail.com>
2019-01-12 23:57:28 +0200

    fix get embedding

--- src/dynamic_meta_embeddings.py
+++ src/dynamic_meta_embeddings.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import torch.nn as nn
 from torch.nn.utils.rnn import *
 import torch.nn.functional as F
@@ -15,16 +16,16 @@ GLOVE_DIM = 300
 FAST_TEXT_DIM = 300
 
 
-def get_embedding(embedding_path, embedding_size, vocab2id):
-    pretrained_embedding = load_embedding(embedding_path)
+def get_embedding(pretrained_embedding, embedding_size, vocab2id):
     embedding = nn.Embedding(num_embeddings=len(vocab2id), embedding_dim=embedding_size)
-    for id, word in vocab2id.items():
+    data = embedding.weight.data
+    for word, id in vocab2id.items():
         if word in pretrained_embedding:
-            embedding[id] = pretrained_embedding[word].mean(0)
+            data[id] = pretrained_embedding[word].mean(0)
         elif word.lower() in pretrained_embedding:
-            embedding[id] = pretrained_embedding[word].mean(0)
+            data[id] = pretrained_embedding[word.lower()].mean(0)
         else:
-            embedding[id] = 0
+            data[id] = 0
     return embedding
 
 
@@ -44,3 +45,13 @@ class UnweightedDME(nn.Module):
         stacked = torch.stack([out_glove, out_fast_text])
         output = torch.sum(stacked)
         return output
+
+
+if __name__ == '__main__':
+
+    glove_path = '../checkpoints/cache/glove.840B.300d.txt.pkl'
+    pretrained_embedding = load_embedding(glove_path)
+
+    vocab_path = '../checkpoints/cache/vocab.pkl'
+    vocab2id = get_vocab2id(vocab_path)
+
--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,10 @@
 import pickle
-
+import torch
+from torchtext import data
 
 def load_embedding(path):
     with open(path, 'rb') as e:
-        embedding = pickle.load(e, encoding='bytes')
+        embedding = pickle.load(e)
     return embedding
 
 
@@ -11,6 +12,12 @@ def get_embeds_vocab(path):
     with open(path, 'rb') as v:
         vocab = pickle.load(v)
     vocab2id = {word: i for i, word in enumerate(list(vocab))}
+    return vocab2id
+
+
+def get_vocab2id(path):
+    with open(path, 'rb') as v:
+        vocab2id = pickle.load(v)
     return vocab2id
 
 
